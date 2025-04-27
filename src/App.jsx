c86d4c38b74c90ec0bd837f9e6ccb47e470da6e4
Bintang <bintangfauzan1000@gmail.com>
2025-04-27 18:37:08 +0700

    Commit Kedua 4/27/2025 18.36

--- src/App.jsx
+++ src/App.jsx
@@ -6,47 +6,49 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 
 function App() {
-  const [saveInputCityName, setSaveInputCityName] = useState('');
-  const [dataWeather, setDataWeather] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+    const [saveInputCityName, setSaveInputCityName] = useState('');
+    const [dataWeather, setDataWeather] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-  function handleSaveInputCityName(newCityName) {
-    setSaveInputCityName(newCityName); //Cara yang benar untuk menyimpan hanya 1 inputan ps. jangan lupa
-  }
-  console.log(saveInputCityName);
+    function handleSaveInputCityName(newCityName) {
+        setSaveInputCityName(newCityName); //Cara yang benar untuk menyimpan hanya 1 inputan ps. jangan lupa
+    }
+    console.log(saveInputCityName);
 
-  useEffect(() => {
-    if (saveInputCityName === "") return; //Validasi input kosong
-    // Make GET request to fetch data
-    axios
-        .get("http://api.weatherapi.com/v1/current.json?key=a25d2e5eb1bc42e88c5110816252504&q="+ saveInputCityName +"&aqi=no")
-        .then((response) => {
-          setDataWeather(response.data); //Simpan data cuaca
-          setLoading(false); //Mengatur loading ke false
-          console.log(response.data);
+    useEffect(() => {
+        if (saveInputCityName === "") return; //Validasi input kosong
+        // Make GET request to fetch data
+        axios
+            .get("http://api.weatherapi.com/v1/forecast.json?key=a25d2e5eb1bc42e88c5110816252504&q="+ saveInputCityName + "&days=5&aqi=no&alerts=no")
+            .then((response) => {
+                setDataWeather(response.data); //Simpan data cuaca
+                setLoading(false); //Mengatur loading ke false
+                console.log(response.data);
 
-        })
-        .catch((err) => {
-          setError(err.message); //Tangani pesan error
-          setLoading(false); //Mengatur loading
-          console.log(err);
-        });
-  }, [saveInputCityName]); //useEffect akan jalan ketika input berubah
+            })
+            .catch((err) => {
+                setError(err.message); //Tangani pesan error
+                setLoading(true); //Mengatur loading
+                console.log(err);
+            });
+    }, [saveInputCityName]); //useEffect akan jalan ketika input berubah
 
-  console.log(dataWeather)
+    console.log(dataWeather)
 
-    if (dataWeather === null) return <SearchBar onSaveCityName={handleSaveInputCityName}/>
     if (loading) return <div>Loading........</div> //Jika loading(true) tampilkan pesan
+    if (dataWeather === null) return <SearchBar onSaveCityName={handleSaveInputCityName}/>
     if(error) return <div className='text-center'>Error: {error}</div>; //Jika error(true) tampilkan pesan
 
-  return (
-      <>
-        <SearchBar onSaveCityName={handleSaveInputCityName} cityName={dataWeather.location.name}/>
-        <WeatherDetails humidity={Math.round(dataWeather.current.humidity)} windSpeed={Math.round(dataWeather.current.wind_mph)} />
-        <ForecastDetails />
-      </>
-  )
+    console.log("foreCast = "  + dataWeather.forecast.forecastday);
+
+    return (
+        <>
+            <SearchBar onSaveCityName={handleSaveInputCityName} cityName={dataWeather.location.name}/>
+            <WeatherDetails humidity={Math.round(dataWeather.current.humidity)} windSpeed={Math.round(dataWeather.current.wind_mph)} />
+            <ForecastDetails forecastData={dataWeather.forecast.forecastday} />
+        </>
+    )
 }
 
 export default App
--- src/components/ForecastDetails.jsx
+++ src/components/ForecastDetails.jsx
@@ -1,18 +1,20 @@
-export default function ForecastDetails() {
+export default function ForecastDetails({forecastData}) {
+    const dataForecast = forecastData;
+
     return (
         <div className='forecast'>
             <h2 className='forecast-header'>5-Days Forecast</h2>
             <div className='forecast-days'>
-                <div className='forecast-day'>
-                    <p>Monday</p>
-                    <p>Cloudy</p>
-                    <p>12 F</p>
-                </div>
-                <div className='forecast-day'>
-                    <p>Tuesday</p>
-                    <p>Cloudy</p>
-                    <p>12 F</p>
-                </div>
+                {dataForecast.map((day,index) => (
+                    <div key={index} className='forecast-day'>
+                        <p>
+                            {new Date(day.date_epoch * 1000).toLocaleDateString('en-US', {
+                                weekday: 'short',
+                            })}
+                        </p>
+                        <img src={day.day.condition.icon} alt=""/>
+                    </div>
+                ))}
             </div>
         </div>
     )
